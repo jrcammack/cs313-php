d8633a38f288b645feeda37c01fd120173e29cb7
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-09-23 22:56:26 -0600

    added color personalization of div 1

--- web/helloJS.js
+++ web/helloJS.js
@@ -1,3 +1,12 @@
 function buttonClick() {
    alert("Clicked!")
 }
+
+function changeBgColor() {
+   var newColor = document.getElementById("bgColor").value;
+   if (document.getElementById("bgColor").value = null) {
+      return;
+   }
+   document.getElementById("div1").style.backgroundColor = newColor;
+   return;
+}
