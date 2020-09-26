dd65c80a031ccea6cbf30f57537f2927b489881c
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-09-25 22:27:06 -0600

    testing

--- web/homeJS.js
+++ web/homeJS.js
@@ -1,12 +1,12 @@
 function revealFact1() {
-   document.getElementById("infoCol1").innerHTML = "Camping can relieve stress and depression";   
+   document.getElementById("infoCol1").innerHTML = "<div class='well well-lg'>Camping can relieve stress and depression</div>";   
 }
 
 function revealItems() {
    document.getElementById("infoCol2").innerHTML = 
-   "Basic Items to Bring<ul id='itemsNeeded'><li>Tent</li><li>Sleeping Bag</li><li>Outdoor Shoes</li><li>Cookware</li><li>Fire Starters</li><li>Food/Water</li></ul>"  
+   "<div class='well well-lg'>Basic Items to Bring<ul id='itemsNeeded'><li>Tent</li><li>Sleeping Bag</li><li>Outdoor Shoes</li><li>Cookware</li><li>Fire Starters</li><li>Food/Water</li></ul></div>"  
 }
 
 function revealFact2() {
-   document.getElementById("infoCol3").innerHTML = "14% of the U.S. population goes camping each year";   
+   document.getElementById("infoCol3").innerHTML = "<div class='well well-lg'>14% of the U.S. population goes camping each year</div>";   
 }
