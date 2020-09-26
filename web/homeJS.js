f1ebfff1ee171c1e043cd5a4fe43e4b7ab246bcf
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-09-25 22:22:11 -0600

    testing

---
+++ web/homeJS.js
@@ -0,0 +1,12 @@
+function revealFact1() {
+   document.getElementById("infoCol1").innerHTML = "Camping can relieve stress and depression";   
+}
+
+function revealItems() {
+   document.getElementById("infoCol2").innerHTML = 
+   "Basic Items to Bring<ul id='itemsNeeded'><li>Tent</li><li>Sleeping Bag</li><li>Outdoor Shoes</li><li>Cookware</li><li>Fire Starters</li><li>Food/Water</li></ul>"  
+}
+
+function revealFact2() {
+   document.getElementById("infoCol3").innerHTML = "14% of the U.S. population goes camping each year";   
+}
