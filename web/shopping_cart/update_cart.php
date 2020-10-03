fb56078575a2f203c612c1ea043e32412fc156a5
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-10-03 04:03:25 -0600

    updating shopping cart project

--- web/shopping_cart/update_cart.php
+++ web/shopping_cart/update_cart.php
@@ -1,46 +1,45 @@
 <?php
-   echo "got into file";
-   // //start the session
-   // session_start();
+   //start the session
+   session_start();
 
-   // //check if session variable contains products already
-   // if (isset($_SESSION["cart"])) {
+   //check if session variable contains products already
+   if (isset($_SESSION["cart"])) {
 
-   //    //get counter and increment 1
-   //    $currentCounter = $_SESSION["numInCart"];
-   //    $newCounter = $currentCounter + 1;
+      //get counter and increment 1
+      $currentCounter = $_SESSION["numInCart"];
+      $newCounter = $currentCounter + 1;
 
-   //    //get current cart
-   //    $currentCart = $_SESSION["cart"];
+      //get current cart
+      $currentCart = $_SESSION["cart"];
 
-   //    //get product to be added
-   //    $productToAdd = $_GET["product"];
+      //get product to be added
+      $productToAdd = $_GET["product"];
 
-   //    //add new product to array
-   //    array_push($currentCart, $productToAdd);
+      //add new product to array
+      array_push($currentCart, $productToAdd);
 
-   //    //set session cart variable equal to new array
-   //    $_SESSION["cart"] = $currentCart;
-   //    $_SESSION["numInCart"] = $newCounter;
-   // }
+      //set session cart variable equal to new array
+      $_SESSION["cart"] = $currentCart;
+      $_SESSION["numInCart"] = $newCounter;
+   }
 
-   // else {
-   //    //create counter to keep track of num in cart
-   //    $counter = 1;
+   else {
+      //create counter to keep track of num in cart
+      $counter = 1;
 
-   //    //create array for session variable
-   //    $productArray = new array();
+      //create array for session variable
+      $productArray = new array();
 
-   //    //get product to be added
-   //    $productToAdd = $_GET["product"];
+      //get product to be added
+      $productToAdd = $_GET["product"];
 
-   //    //add new product to array
-   //    array_push($productArray, $productToAdd);
+      //add new product to array
+      array_push($productArray, $productToAdd);
 
-   //    //set session cart variable equal to new array
-   //    $_SESSION["cart"] = $productArray;
-   //    $_SESSION["numInCart"] = $counter;   
-   // }
+      //set session cart variable equal to new array
+      $_SESSION["cart"] = $productArray;
+      $_SESSION["numInCart"] = $counter;   
+   }
    
-   // echo $_SESSION["numInCart"];
+   echo $_SESSION["numInCart"];
 ?>
