e90875602725f4d762a784b7dcd13edba9f71828
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-10-03 19:37:40 -0600

    updating shopping cart project

--- web/shopping_cart/checkout.php
+++ web/shopping_cart/checkout.php
@@ -312,13 +312,14 @@
          </div>
       </div>
       <div id="infoRow" class="row">
-         <div id="" class="col-lg-8"><div class=""></div></div>
+         <div id="" class="col-lg-6"><div class=""></div></div>
+         <div id="infoCol1" class="col-lg-2"><div id="numInCart" class="well well-lg">Total: $<?php echo ($_SESSION["tent"]->getTotalPrice() + $_SESSION["camping chair"]->getTotalPrice() + $_SESSION["cookware"]->getTotalPrice() + $_SESSION["cooler"]->getTotalPrice() + $_SESSION["flashlight"]->getTotalPrice() + $_SESSION["hammock"]->getTotalPrice() + $_SESSION["hiking backpack"]->getTotalPrice() + $_SESSION["mountain bike"]->getTotalPrice() + $_SESSION["sleeping bag"]->getTotalPrice()); ?></div></div>
          <div id="infoCol1" class="col-lg-2"><div id="numInCart" class="well well-lg">Number of Items in Cart: <?php echo $_SESSION["numInCart"] ?></div></div>
          <div id="infoCol1" class="col-lg-2"><div class=""><a href="view_cart.php" class="btn btn-primary">Return to Cart</a></div></div>
       </div>
       <div id="info" class="container-fluid">
          <div id="" class="col-lg-12">
-            <div class="">
+            <div class="well well-lg">
                <form method="POST" action="confirmation.php" id="completionForm">
                   Steet:<input type="text" name="street"><br>
                   City:<input type="text" name="city"><br>
--- web/shopping_cart/view_cart.php
+++ web/shopping_cart/view_cart.php
@@ -313,9 +313,10 @@
       </div>
       <div id="infoRow" class="row">
          <div id="" class="col-lg-6"><div class=""></div></div>
+         <div id="infoCol1" class="col-lg-2"><div id="numInCart" class="well well-lg">Total: $<?php echo ($_SESSION["tent"]->getTotalPrice() + $_SESSION["camping chair"]->getTotalPrice() + $_SESSION["cookware"]->getTotalPrice() + $_SESSION["cooler"]->getTotalPrice() + $_SESSION["flashlight"]->getTotalPrice() + $_SESSION["hammock"]->getTotalPrice() + $_SESSION["hiking backpack"]->getTotalPrice() + $_SESSION["mountain bike"]->getTotalPrice() + $_SESSION["sleeping bag"]->getTotalPrice()); ?></div></div>
          <div id="infoCol1" class="col-lg-2"><div id="numInCart" class="well well-lg">Number of Items in Cart: <?php echo $_SESSION["numInCart"] ?></div></div>
-         <div id="infoCol1" class="col-lg-2"><div class=""><a href="browse.php" class="btn btn-primary">Return to Browse</a></div></div>
-         <div id="infoCol1" class="col-lg-2"><div class=""><a href="checkout.php" class="btn btn-primary">Checkout</a></div></div>
+         <div id="infoCol1" class="col-lg-1"><div class=""><a href="browse.php" class="btn btn-primary">Return to Browse</a></div></div>
+         <div id="infoCol1" class="col-lg-1"><div class=""><a href="checkout.php" class="btn btn-primary">Checkout</a></div></div>
       </div>
       <div id="info" class="container-fluid">
          <div id="infoRow" class="row">
