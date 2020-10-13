9e75018bb12424357ccf9aefff9c639ba4171e68
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-10-13 08:07:44 -0600

    fixing book search functionality

--- web/book_search.php
+++ web/book_search.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
    <head>
-      <title>The Camping Guide</title>
+      <title>Scripture Finder</title>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <script type="text/javascript" src="homeJS.js"></script>
