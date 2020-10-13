cc00ade30f354dfbf6fbdd96cac11ec41c7a52e5
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-10-13 00:34:05 -0600

    added php code to query experimental tables and retrieve info

--- web/team_prac_w05.php
+++ web/team_prac_w05.php
@@ -30,7 +30,34 @@
       <div id="intro" class="container-fluid">
          <div id="introRow" class="row">
             <div id="introCol" class="col-lg-12">
-                
+               <?php
+                  try
+                  {
+                    $dbUrl = getenv('DATABASE_URL');
+                  
+                    $dbOpts = parse_url($dbUrl);
+                  
+                    $dbHost = $dbOpts["host"];
+                    $dbPort = $dbOpts["port"];
+                    $dbUser = $dbOpts["user"];
+                    $dbPassword = $dbOpts["pass"];
+                    $dbName = ltrim($dbOpts["path"],'/');
+                  
+                    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                  
+                    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                  }
+                  catch (PDOException $ex)
+                  {
+                    echo 'Error!: ' . $ex->getMessage();
+                    die();
+                  }
+
+                  foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+                  {
+                     echo '<b>' . $row['book'] . $row['chapter'] . ':' . $row['verse'] . '</b>' . '-' . $row['content'] . '<br';
+                  }
+               ?>
             </div>
          </div>
       </div>      
