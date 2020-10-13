4d210f79fb21667992157769e8d29e72d04f8144
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-10-13 08:11:24 -0600

    fixing book search functionality

--- web/book_search.php
+++ web/book_search.php
@@ -56,7 +56,7 @@
                   // get book variable to search for
                   $book = $_POST['book'];
 
-                  $statement = $db->query("SELECT book, chapter, verse, content FROM scriptures WHERE LOWER(book) = LOWER($book)");
+                  $statement = $db->query("SELECT book, chapter, verse, content FROM scriptures");
                   while ($row = $statement->fetch(PDO::FETCH_ASSOC))
                   {
                      echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse'] . '</b>' . ' ' . '-' . ' ' . '"' . $row['content'] . '"' . '<br><br>';
