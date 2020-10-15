39d816af0e45ef6c8004fbab10569fb6511251bb
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-10-15 17:53:05 -0600

    worked on login page style

--- web/golf_app/golf_homepage.php
+++ web/golf_app/golf_homepage.php
@@ -26,17 +26,12 @@
       </nav>
       <div id="homeLogin" class="container-fluid">
          <div class="row">
-         <div class="col-lg-4"></div>
-            <div class="col-lg-1">
+            <div class="col-lg-4"></div>
+            <div class="col-lg-4">
                <div class="well well-lg">
-                  <a href="" class="btn btn-success">LOGIN</a>  
+                  <a href="" class="btn btn-success">LOGIN</a>
+                  <a href="" class="btn btn-default disabled">REGISTER</a>
                </div>
-            </div>
-            <div class="col-lg-2"></div>
-            <div class="col-lg-1">
-               <div class="well well-lg">
-                  <a href="" class="btn disabled">REGISTER</a>  
-               </div>   
             </div>
             <div class="col-lg-4"></div>
          </div>
