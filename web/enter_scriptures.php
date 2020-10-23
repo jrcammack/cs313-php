7d5229620e95c21a618e436145b2e6dead3c1dd5
Jonathan Cammack <jonathanrcammack@gmail.com>
2020-10-22 19:06:37 -0600

    checking to see where code issue is

--- web/enter_scriptures.php
+++ web/enter_scriptures.php
@@ -37,39 +37,39 @@
                   Content:<textarea name="content" rows="5" cols="60"></textarea><br>
 
                   <?php
-                     try
-                     {
-                        $dbUrl = getenv('DATABASE_URL');
+                  //    try
+                  //    {
+                  //       $dbUrl = getenv('DATABASE_URL');
                      
-                        $dbOpts = parse_url($dbUrl);
+                  //       $dbOpts = parse_url($dbUrl);
                      
-                        $dbHost = $dbOpts["host"];
-                        $dbPort = $dbOpts["port"];
-                        $dbUser = $dbOpts["user"];
-                        $dbPassword = $dbOpts["pass"];
-                        $dbName = ltrim($dbOpts["path"],'/');
+                  //       $dbHost = $dbOpts["host"];
+                  //       $dbPort = $dbOpts["port"];
+                  //       $dbUser = $dbOpts["user"];
+                  //       $dbPassword = $dbOpts["pass"];
+                  //       $dbName = ltrim($dbOpts["path"],'/');
                      
-                        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+                  //       $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
                      
-                        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                     }
-                     catch (PDOException $ex)
-                     {
-                        echo 'Error!: ' . $ex->getMessage();
-                        die();
-                     }
+                  //       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                  //    }
+                  //    catch (PDOException $ex)
+                  //    {
+                  //       echo 'Error!: ' . $ex->getMessage();
+                  //       die();
+                  //    }
                   
-                     $statement = $db->prepare("SELECT id, name FROM topic");
-                     $statement->execute();
+                  //    $statement = $db->prepare("SELECT id, name FROM topic");
+                  //    $statement->execute();
                   
-                     while ($row = $statement->fetch(PDO::FETCH_ASSOC))
-                     {
-                        $id = $row['id'];
-                        $name = $row['name'];
-                        echo "<input type='checkbox' name='topics' id='topics$id' value='$id'>";
-                        echo "<label for='topics$id'>$name</label><br>"
-                     }
-                  ?>
+                  //    while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+                  //    {
+                  //       $id = $row['id'];
+                  //       $name = $row['name'];
+                  //       echo "<input type='checkbox' name='topics' id='topics$id' value='$id'>";
+                  //       echo "<label for='topics$id'>$name</label><br>"
+                  //    }
+                  // ?>
                   <button type="submit">Submit</button>
                </form> 
             </div>
